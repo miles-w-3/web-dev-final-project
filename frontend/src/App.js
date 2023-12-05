5173a867d2b822e4d11f6a6968bee1e3482bed93
Miles Wilson <wilson.mil@icloud.com>
2023-12-05 16:05:15 -0500

    Add routing and auth screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PlacesAutocompleteComponent from './webpages/test';
+import UserAuth from './webpages/auth';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
     return (
-        <div className="App">
-            <PlacesAutocompleteComponent />
-        </div>
+        <ChakraProvider>
+            <div className="App">
+                <Router>
+                    <Routes>
+                        <Route path='/search' element={<PlacesAutocompleteComponent />} />
+                        <Route path='/auth' element={<UserAuth />} />
+                    </Routes>
+                </Router>
+            </div>
+        </ChakraProvider>
     );
 }
 
